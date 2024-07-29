e5d3ff05765d2a29ad482766b73e205890b03980
shayan-zafar <160698212+shayan-zafar@users.noreply.github.com>
2024-07-29 14:03:06 +0500

    commit2

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,21 +1,9 @@
-import AppName from "./components/AppName";
-import Addtodo from "./components/Addtodo";
-import Todoitem1 from "./components/Todoitem-1";
-import Todoitem2 from "./components/Todoitem-2";
-import "./App.css";
 function App() {
-  return (
-    <>
-      <center className="container">
-        <AppName />
-        <Addtodo />
-        <div className="item-container ">
-          <Todoitem1 />
-          <Todoitem2 />
-        </div>
-      </center>
-    </>
-  );
-}
+    return (
+      <>
+       <h1>REAL ESTATE WEBSITE</h1>
+      </>
+    );
+  }
 
-export default App;
+  export default App;
--- web/src/components/AppName.jsx
+++
@@ -1,4 +0,0 @@
-function AppName() {
-  return <h1> TO DO APP in react</h1>;
-}
-export default AppName;
